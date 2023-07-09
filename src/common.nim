328875d4945339713024de037b797baacc2f0101
Mads Hougesen <mads@mhouge.dk>
2023-07-09 11:51:32 +0200

    feat: sort files (#6)

--- src/common.nim
+++ src/common.nim
@@ -1,4 +1,6 @@
 from std/os import pcFile, walkDir, getHomeDir, joinPath
+from std/strutils import parseInt
+
 
 proc getFileNames*(folderPath: string): seq[string] =
     var files: seq[string] = @[]
@@ -12,3 +14,13 @@ proc getFileNames*(folderPath: string): seq[string] =
 
 proc getSavePath*(): string =
     return joinPath(getHomeDir(), ".remember")
+
+
+proc sortStringsByNumber*(a: string, b: string): int =
+    let aInt: int64 = parseInt(a)
+    let bInt: int64 = parseInt(b)
+
+    if aInt > bInt:
+        return 1
+
+    return -1
